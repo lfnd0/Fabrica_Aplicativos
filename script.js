db33884df88b485226f40dbdbfdbba507cf6139c
luizfernando <luiz-fsilva37@hotmail.com>
2019-12-22 18:33:05 -0300

    Adição módulo 1

---
+++ script.js
@@ -0,0 +1,45 @@
+function entrar() {
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome: ');
+
+    if (texto == '' || texto == null) {
+        alert('Digite seu nome novamente!');
+        area.innerHTML = 'Bem vindo visitante!';
+    } else {
+        area.innerHTML = 'Bem vindo ' + texto + '!';
+    }
+}
+
+document.write("Valores para x com while:");
+x = 0;
+while (x <= 10) {
+    document.write("<br> x = " + x);
+    x++;
+}
+
+document.write("<br><br>Valores para y com for:");
+
+for (y = 0; y <= 10; y++) {
+    document.write("<br> y = " + y);
+}
+
+document.write("<br> Escolha seu pedido: <br>")
+document.write("<br> 0 - Sorvete | 1 - Suco de fruta | 2 - Refrigerante <br>")
+document.write("<br> Escolha seu pedido: <br>")
+
+function pedir() {
+    z = prompt("Digite seu pedido: ");
+    switch (z) {
+        case "0":
+            alert("Voce pediu um sorvete!");
+            break;
+        case "1":
+            alert("Voce pediu um suco de fruta!");
+            break;
+        case "2":
+            alert("Voce pediu um refrigerante!");
+            break;
+        default:
+            alert("Obs, pedido nao encontrado! Tente de novo.")
+    }
+}
