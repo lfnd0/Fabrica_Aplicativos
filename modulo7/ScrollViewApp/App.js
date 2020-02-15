8702caf8155f2211959606900cd3b4593aad5b06
luizfernando <luiz-fsilva37@hotmail.com>
2020-02-15 12:47:23 -0300

    Adição módulo 7 final ScrollViewApp

--- modulo7/ScrollViewApp/App.js
+++ modulo7/ScrollViewApp/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+      </ScrollView>
     </View>
   );
 }
@@ -12,8 +17,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  box1: {
+    backgroundColor: 'red',
+    height: 250
+  },
+  box2: {
+    backgroundColor: 'green',
+    height: 250
+  },
+  box3: {
+    backgroundColor: 'yellow',
+    height: 250
+  },
+  box4: {
+    backgroundColor: 'blue',
+    height: 250
   },
 });
