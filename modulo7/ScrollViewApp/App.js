8cbf7c94da6e3849d12ae35f0a44e0c6fc903394
luizfernando <luiz-fsilva37@hotmail.com>
2020-02-29 23:33:48 -0300

    Atualização módulo 7 ScrollViewApp

--- modulo7/ScrollViewApp/App.js
+++ modulo7/ScrollViewApp/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
@@ -33,5 +33,5 @@ const styles = StyleSheet.create({
   box4: {
     backgroundColor: 'blue',
     height: 250
-  },
+  }
 });
